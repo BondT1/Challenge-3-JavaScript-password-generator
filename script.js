df4c44fd798a5c313448755d5e78269976ed932c
Tom Bond <tombond248@gmail.com>
2022-10-11 21:19:42 +0100

    created new password generator function

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  const password = generatePassword();
   const passwordText = document.querySelector("#password");
-  const passwordCriteria = prompt("Password-Criteria");
+  const passwordCriteria = prompt ("Password Criteria");
   alert (passwordCriteria);
-
 
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  const password = generatePassword();
+  return "password"
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
+ 
 
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
