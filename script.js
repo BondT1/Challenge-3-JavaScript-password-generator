eed9c82852d37063b585b6e5a53e90b4dfb2e678
Tom Bond <tombond248@gmail.com>
2022-10-12 19:24:38 +0100

    completed additional arrays and started to complete length function

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", writePassword);
+
+
+// Different Arrays
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789";
+const symbols = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
+
+var verifyLength = "";
+
+
 
 // Write password to the #password input
 function writePassword() {
   const passwordText = document.querySelector("#password");
-  const passwordCriteria = prompt ("Password Criteria");
-  alert (passwordCriteria);
+  const password = generatePassword();
 
   passwordText.value = password;
 
 }
 
-function generatePassword() {
-  const password = generatePassword();
-  return "password"
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
+function generatePassword() {
+  
+  const verifyLength = prompt ("Please specify how many characters you want your password to contain");
+  alert (verifyLength);
+  return password;
+
+  
+  if(verifyLength > 128 && verifyLength < 8 ) {
+    alert("Password length must be between 8 and 128 characters");
+  }
+}
  
 
-const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers = "0123456789";
-const symbols = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
+
 
 
