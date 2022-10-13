1e9bc8d8ed81173a430856eb1891e28fc397f0b1
Tom Bond <tombond248@gmail.com>
2022-10-13 17:17:22 +0100

    created randomizer for final password

--- script.js
+++ script.js
@@ -4,29 +4,28 @@ const generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// Different Arrays
-const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers = "0123456789";
-const specialCharacters = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
-const finalPassword = ""
-
 // Write password to the #password input
 function writePassword() {
-  const passwordText = document.querySelector("#password");
-  const password = generatePassword();
-
+  var passwordText = document.querySelector("#password");
+  var password = generatePassword();
   passwordText.value = password;
-
 }
+
+// Different variables
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
+var passwordCharacters = ""
 
 
 function generatePassword() {
-  
+ 
+  // Prompt for the number of characters in the password 
   var verifyLength = prompt ("Please specify how many characters you want your password to contain. (8 - 128 characters)");
   alert (verifyLength);
 
+  // Loops back round to the above prompt if the criteria is not met
   while(verifyLength <= 7 || verifyLength >= 127 ) {
     alert("Password length must be between 8 and 128 characters");
     var verifyLength = prompt ("Please specify how many characters you want your password to contain. (8 - 128 characters)");
@@ -36,10 +35,11 @@ function generatePassword() {
     alert("Enter a valid number.");
     var verifyLength = prompt ("Please specify how many characters you want your password to contain. (8 - 128 characters)");
   }
-    
+
+  // Confirms password length to user 
   alert("Your password will be " + verifyLength + " characters long.");
   
-
+  //Prompts for the different parameters within the password
   var verifyUppercase = confirm ("Do you want uppercase characters in your password?");
   if (verifyUppercase) {
     alert ("Your password WILL contain uppercase characters");
@@ -68,6 +68,7 @@ function generatePassword() {
     alert ("Your password WILL NOT contain special characters");
   }
 
+  // loops to first prompt if no criteria is selected - however does not include alerts as need to add these in - may make code too long winded though
   while(verifyUppercase === false && verifyLowercase === false && verifyNumbers === false && verifySpecial === false) {
     alert("At least one criteria must be selected")
     var verifyUppercase = confirm ("Do you want uppercase characters in your password?");
@@ -75,24 +76,29 @@ function generatePassword() {
     var verifyNumbers = confirm ("Do you want numbers in your password?");
     var verifySpecial = confirm ("Do you want special characters in your password?");
   }
-
-  // Might not use the alerts for the above 4 variables due to it having to be very long winded when creating the above loop
-
+  
+  // Arrays grouped together to create password criteria selections
   if (verifyUppercase) {
-    finalPassword = finalPassword.concat(uppercaseLetters);
+    passwordCharacters = passwordCharacters.concat(uppercaseLetters);
   }
   if (verifyLowercase) {
-    finalPassword = finalPassword.concat(lowercaseLetters);
+    passwordCharacters = passwordCharacters.concat(lowercaseLetters);
   }
   if (verifyNumbers) {
-    finalPassword = finalPassword.concat(numbers);
+    passwordCharacters = passwordCharacters.concat(numbers);
   }
   if (verifySpecial) {
-    finalPassword = finalPassword.concat(specialCharacters);
+    passwordCharacters = passwordCharacters.concat(specialCharacters);
   }
 
-
+  // randomizes characters with specified length to create final password
+  let confirmedPassword = ""
+  for (let i = 0; i < verifyLength; i++) {
+    let random =[Math.floor(Math.random() * passwordCharacters.length)];
+    confirmedPassword = confirmedPassword + passwordCharacters[random];
   }
+  return confirmedPassword;
+};
     
   
 
