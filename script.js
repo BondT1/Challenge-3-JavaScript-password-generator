543388547298c541da89506e5cfcdf0c2295b941
Tom Bond <tombond248@gmail.com>
2022-10-14 13:07:24 +0100

    creating a neater and faster way of coding the criteria and loop

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// Assignment Code
+// Different variables
 const generateBtn = document.querySelector("#generate");
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialCharacters = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -11,16 +13,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Different variables
-var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var specialCharacters = " !#$%&'()*+,-./:;<=>?@][\^_`{|}~";
-var passwordCharacters = ""
-
-
-function generatePassword() {
- 
+function verifyLength() {
   // Prompt for the number of characters in the password 
   var verifyLength = prompt ("Please specify how many characters you want your password to contain. (8 - 128 characters)");
 
@@ -35,45 +28,60 @@ function generatePassword() {
     var verifyLength = prompt ("Please specify how many characters you want your password to contain. (8 - 128 characters)");
   }
 
-  // Confirms password length to user 
-  alert("Your password will be " + verifyLength + " characters long.");
+  return verifyLength;
+} 
+
+function generatePassword() {
+  const passwordLength = verifyLength();
+  var passwordCharacters = "";
+
+  // // Confirms password length to user 
+  // alert("Your password will be " + verifyLength + " characters long.");
   
-  //Prompts for the different parameters within the password
-  var verifyUppercase = confirm ("Do you want uppercase characters in your password?");
-  if (verifyUppercase) {
-    alert ("Your password WILL contain uppercase characters");
-  } else {
-    alert ("Your password WILL NOT contain uppercase characters");
-  }
+  // //Prompts for the different parameters within the password
+  // var verifyUppercase = confirm ("Do you want uppercase characters in your password?");
+  // if (verifyUppercase) {
+  //   alert ("Your password WILL contain uppercase characters");
+  // } else {
+  //   alert ("Your password WILL NOT contain uppercase characters");
+  // }
 
-  var verifyLowercase = confirm ("Do you want lowercase characters in your password?");
-  if (verifyLowercase) {
-    alert ("Your password WILL contain lowercase characters");
-  } else {
-    alert ("Your password WILL NOT contain lowercase characters");
-  }
+  // var verifyLowercase = confirm ("Do you want lowercase characters in your password?");
+  // if (verifyLowercase) {
+  //   alert ("Your password WILL contain lowercase characters");
+  // } else {
+  //   alert ("Your password WILL NOT contain lowercase characters");
+  // }
 
-  var verifyNumbers = confirm ("Do you want numbers in your password?");
-  if (verifyNumbers) {
-    alert ("Your password WILL contain numbers");
-  } else {
-    alert ("Your password WILL NOT contain numbers");
-  }
+  // var verifyNumbers = confirm ("Do you want numbers in your password?");
+  // if (verifyNumbers) {
+  //   alert ("Your password WILL contain numbers");
+  // } else {
+  //   alert ("Your password WILL NOT contain numbers");
+  // }
 
-  var verifySpecial = confirm ("Do you want special characters in your password?");
-  if (verifySpecial) {
-    alert ("Your password WILL contain special characters");
-  } else {
-    alert ("Your password WILL NOT contain special characters");
-  }
+  // var verifySpecial = confirm ("Do you want special characters in your password?");
+  // if (verifySpecial) {
+  //   alert ("Your password WILL contain special characters");
+  // } else {
+  //   alert ("Your password WILL NOT contain special characters");
+  // }
+
+  var verifyUppercase = verifyLowercase = verifyNumbers = verifySpecial = false;
+  var count = 0;
 
   // loops to first prompt if no criteria is selected - however does not include alerts as need to add these in - may make code too long winded though
   while(verifyUppercase === false && verifyLowercase === false && verifyNumbers === false && verifySpecial === false) {
-    alert("At least one criteria must be selected")
-    var verifyUppercase = confirm ("Do you want uppercase characters in your password?");
-    var verifyLowercase = confirm ("Do you want lowercase characters in your password?");
-    var verifyNumbers = confirm ("Do you want numbers in your password?");
-    var verifySpecial = confirm ("Do you want special characters in your password?");
+    if(count > 0) {
+      alert("At least one criteria must be selected");
+    }
+  
+    verifyUppercase = confirm ("Do you want uppercase characters in your password?");
+    verifyLowercase = confirm ("Do you want lowercase characters in your password?");
+    verifyNumbers = confirm ("Do you want numbers in your password?");
+    verifySpecial = confirm ("Do you want special characters in your password?");
+
+    count++
   }
   
   // Arrays grouped together to create password criteria selections
@@ -92,12 +100,15 @@ function generatePassword() {
 
   // randomizes characters with specified length to create final password
   let confirmedPassword = ""
-  for (let i = 0; i < verifyLength; i++) {
+  for (let i = 0; i < passwordLength; i++) {
     let random =[Math.floor(Math.random() * passwordCharacters.length)];
     confirmedPassword = confirmedPassword + passwordCharacters[random];
   }
   return confirmedPassword;
 };
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
     
   
 
